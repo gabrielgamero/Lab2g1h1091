c7f0acf813e933a7dc8dd17d1bd1464e3bf9a0bf
Eduardo <eduardo.requena@pucp.pe>
2020-05-18 19:49:27 -0500

    actualizado lista_departamentos

--- app/src/main/java/com/example/lab2g1h1091/MainActivity.java
+++ app/src/main/java/com/example/lab2g1h1091/MainActivity.java
@@ -12,19 +12,6 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.appbar_lista_trabajo,menu);
-        return true;
-    }
-
-
-
-    public void abrirRegistro(MenuItem item) {
-            Intent i = new Intent(this,CrearTrabajo.class);
-            startActivity(i);
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
